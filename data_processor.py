3155b9cb8a791f4f437569f8def018704c2137ab
Alexander Bodalevsky <alexander.bodalevsky@capgemini.com>
2025-03-03 18:58:02 +0200

    Added data processor

---
+++ data_processor.py
@@ -0,0 +1,15 @@
+"""
+Module: DataBriks PySpark Processor
+Description: This script is used to process data using PySpark on DataBriks
+"""
+
+import sys
+import os
+
+def main():
+    """Main entry point of the script."""
+    # Your code here
+    pass
+
+if __name__ == "__main__":
+    main()
